c455ecf6df3991a00d25e87b07830de8e761096b
SeraajDV <seraajdevilliers@gmail.com>
2024-09-30 23:07:55 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  }
+})
